cc4948099439e4399e59ab3f447e4231019dab47
Funmi123 <ajanakueniola@gmail.com>
2026-02-08 01:51:07 +0100

    files and folders created for the Git-Action pipeline3

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,11 @@
-output "nginx_public_ip" {
-  value = aws_instance.nginx.public_ip
+output "nginx_public_ips" {
+  value = aws_instance.nginx[*].public_ip
 }
 
-output "java_public_ip" {
-  value = aws_instance.java.public_ip
+output "app_private_ips" {
+  value = aws_instance.app[*].private_ip
+}
+
+output "postgres_endpoint" {
+  value = aws_db_instance.postgres.endpoint
 }
